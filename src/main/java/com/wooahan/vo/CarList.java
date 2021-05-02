46ab914379e326e53118f5973263680d57ecd8b7
JSA <sa.cho@newploy.net>
2021-05-03 01:25:45 +0900

    feat($pre-game): 자동차 목록 검증하기
    1. 자동차 이름 검증
    2. 자동차 목록 사이즈 검증

--- src/main/java/com/wooahan/vo/CarList.java
+++ src/main/java/com/wooahan/vo/CarList.java
@@ -13,7 +13,7 @@ public class CarList {
     private List<Car> carList;
 
     public CarList(String carNames) throws InputMismatchException {
-        String[] cars = carNames.split(ValueConstants.WINNER_DELIMITER_CHAR);
+        String[] cars = validateCars(carNames);
         setCarList(cars);
     }
 
@@ -21,6 +21,37 @@ public class CarList {
         carList = new ArrayList<>();
         for (String name : cars) {
             carList.add(new Car(name));
+        }
+    }
+
+    private String[] validateCars(String carNames) throws InputMismatchException {
+        String[] rawCars = carNames.split(ValueConstants.WINNER_DELIMITER_CHAR);
+        validateListSize(rawCars);
+        String[] cars = new String[rawCars.length];
+        for (int idx = 0; idx < rawCars.length; idx++) {
+            String name = rawCars[idx].trim();
+            validateEmptyString(name);
+            validateLength(name);
+            cars[idx] = name;
+        }
+        return cars;
+    }
+
+    private void validateEmptyString(String name) throws InputMismatchException {
+        if (name == null || "".equals(name)) {
+            throw new InputMismatchException();
+        }
+    }
+
+    private void validateLength(String name) throws InputMismatchException {
+        if (name.length() > ValueConstants.CAR_NAME_LIMIT) {
+            throw new InputMismatchException();
+        }
+    }
+
+    private void validateListSize(String[] rawCars) throws InputMismatchException {
+        if (rawCars.length > ValueConstants.CAR_LIST_LIMIT) {
+            throw new InputMismatchException();
         }
     }
 }
